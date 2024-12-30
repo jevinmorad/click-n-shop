e3a450b9a7dcecf24452ee9f3990bd394d579c95
jevinmorad <23010101171@darshan.ac.in>
2024-12-30 23:58:15 +0530

    Implement layout structure with Navbar, footer, and search functionality

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,8 +1,18 @@
-import { Outlet, Link } from "react-router-dom"
-
-export default function Layout(){
-    return(<>
-        <div className="text-center p-6 bg-slate-400 font-sans text-black">this is layout page</div>
-    </>)
+import { Outlet } from "react-router-dom"
+import Navbar from "./navbar/Navbar"
+export default function Layout() {
+    return (
+        <>
+            <nav>
+                <Navbar />
+            </nav>
+            <main>
+                <Outlet />
+            </main>
+            <footer>
+                <p>&copy; 2021</p>
+            </footer>
+        </>
+    )
 }
 
