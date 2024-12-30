e0ceaf3191c1ad60268afc434606a0a13c579c42
Prashamtogadiya <pktogadiya222@gmail.com>
2024-12-30 19:26:24 +0530

    Added Tailwind configurations

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -2,7 +2,7 @@ import { Outlet, Link } from "react-router-dom"
 
 export default function Layout(){
     return(<>
-        <div>this is layout page</div>
+        <div className="text-center p-6 bg-slate-400 font-sans text-black">this is layout page</div>
     </>)
 }
 
