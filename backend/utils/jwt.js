7e92b97bb233deb08dae5fbfd88d205025db11c6
jevinmorad <23010101171@darshan.ac.in>
2025-01-04 10:38:53 +0530

    Add JWT utility for token generation

---
+++ backend/utils/jwt.js
@@ -0,0 +1,7 @@
+const jwt = require("jsonwebtoken")
+
+const generateToken = (id, isAdmin) => {
+    return jwt.sign({ id, isAdmin }, process.env.JWT_SECRET, { expiresIn: '30d' })
+}
+
+module.exports = { generateToken }
