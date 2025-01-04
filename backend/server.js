48b6a2fe66249b3063379750a4f2da9905ebc7e9
jevinmorad <23010101171@darshan.ac.in>
2025-01-04 10:39:51 +0530

    Add user routes and error handling middleware to server setup

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express()
+const userRoutes = require('./routes/user-route')
 const productRoutes = require('./routes/product-route');
+const route = require('./routes/user-route');
+const { errorHandler } = require('./middleware/error-middleware');
 const PORT = process.env.PORT || 3000;
 
-connectDB()
+connectDB(); // Connect with database
 
-app.use(express.json())
+// Middleware
+app.use(express.json()) // Parse json data
+app.use(errorHandler) // Error handling 
 
-
-app.use('/api/products', productRoutes); 
-
-
+// Routes
+app.use('/user', userRoutes);
+app.use('/products', productRoutes); 
 
 app.listen(PORT,()=>{
     console.log("Server Started");    
